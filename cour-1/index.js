b0c4abd5352b71129c83de0a0e13179b2d7f89ae
naoualloli <naoualloli967@gmail.com>
2024-01-30 09:37:00 +0100

    test git

---
+++ cour-1/index.js
@@ -0,0 +1,31 @@
+// console.log("Javascript")
+// Les fonction en javascript
+// function greet(name){
+//     // console.log("hellooo" + name)
+// }
+
+//  greet(" naoual")
+// function somme(nombre1 , nombre2){
+//     return nombre1+nombre2
+// }
+// console.log (somme (13,de))
+
+
+ // declaration des variables
+  const PI = 3.14
+  function inscrire(){
+     let name;
+     let prenom;
+     let age;
+     name = "eladadnani"
+     prenom = "naoual"
+     age = 21
+     if (age < 22){
+             var statut
+         statut = "stagiaire"
+     }
+     console.log(name)
+     console.log(statut)
+}
+inscrire()
+
---
+++ cour-2/index.js
@@ -0,0 +1,18 @@
+// let a =5 
+// let b ='5'
+// if (a==b){
+//     console.log (a+b)
+// }
+// Operateurs de type en javascript
+ let statut="etudiant"
+ let age =20
+ if (statut =="etudiant" && age>20) {
+        console.log("OK")
+ }else{console.log("NON")
+} 
+
+
+if (statut="etudiant"|| age==22) {
+    console.log("OK")
+}else{console.log("NON")
+}
