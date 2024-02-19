48f8b5bc5ecb09cf81a49d541ec933a64fa61bab
naoualloli <naoualloli967@gmail.com>
2024-02-19 17:03:06 +0100

    update for the second test

---
+++ cour-4/index.js
@@ -0,0 +1,88 @@
+console.log(Number("3.14"))         
+console.log(Number(" "))                
+console.log(Number(""))                
+console.log(Number("99 88"))                
+console.log(Number("dd106"))                
+
+let d = 2025
+
+sd = String(d)                  
+
+console.log(sd)
+
+console.log(typeof(sd))
+
+let x = "5"
+
+let y = + x                
+
+console.log(typeof(y))
+
+console.log(String(100 + 23))           
+
+
+console.log(typeof(String(100 + 23)))
+
+console.log(String(100) + String(23))    
+
+let k = 1000
+
+sk = k.toString()              
+
+console.log(typeof(sk))
+
+
+// conversion des dates en nombres
+
+
+console.log("==>conversion des dates en nombres")
+
+let vD = new Date()
+
+
+console.log(vD)         
+
+
+
+console.log(Number(vD))
+
+console.log(vD.getDate())
+
+console.log(vD.getDay())
+
+
+console.log(typeof(vD.getDay()))
+
+
+console.log(typeof(vD.getDay()).toString())
+
+
+console.log(String(vD.getDate()))
+
+
+console.log(typeof(String(vD.getDate())))
+
+
+console.log(vD.getFullYear())
+
+
+console.log(vD.getHours())
+
+
+
+console.log(vD.getMilliseconds())
+
+
+console.log(vD.getMinutes())
+
+
+
+console.log(vD.getMonth())
+
+
+
+console.log(vD.getSeconds())
+
+
+
+console.log(vD.getTime())
