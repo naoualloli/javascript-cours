ced76bf67dd321842984e97070818cecede56fa8
naoualloli <naoualloli967@gmail.com>
2024-02-29 11:26:32 +0100

    cour-JavaScript

---
+++ cour-10/index.js
@@ -0,0 +1,25 @@
+console.log(document.body.parentNode)
+console.log(document.body.parentElement)
+
+console.log(document.body.childNodes)
+console.log(document.querySelector("nav").nextElementSibling)
+
+console.log(document.querySelector("ul").firstElementChild)
+console.log(document.querySelector("ul").lastElementChild)
+
+//retourne une list ... necessiteune boucle
+console.log(document.querySelector("ul").children)
+console.log(document.querySelector("ul").childNodes)
+
+
+
+
+
+
+
+
+
+
+
+
+
