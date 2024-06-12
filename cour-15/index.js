0f13dea222d6991218fbd42e3e2b8f7336b5aea5
naoualloli <naoualloli967@gmail.com>
2024-06-12 19:26:28 +0200

    cour-15

---
+++ cour-15/index.js
@@ -0,0 +1,132 @@
+// //les objects
+
+// //const stagiare = {id : 1,
+// //                nom:"ELADNANI",
+// //                 prenom :"NAOUAL",
+// //                branche :"DEV",
+// //                presenterVous: function(){
+// ////                   console.log("I am "  +this.nom)
+// //               }
+
+
+
+// //}
+
+
+
+
+
+
+
+// // function Stagiare(v_id , v_nom, v_prenom , v_branche){
+// //     this.id = v_id ;
+// //     this.prenom = v_nom ;
+// //     this.nom = v_prenom ;
+// //     this.branche = v_branche ;
+
+// //     this.presenterVous = function(){
+// //         console.log ( " I am " + this.nom +" " + this.prenom) ;
+// //     }
+// // }
+
+// // const stagiare1 = new Stagiare(1, "mribeh" , " hiba ", "dev")
+// // console.log(stagiare1)
+
+
+// //EXERCICE
+
+// // function Achat (a_id , a_article , a_prix , a_qte){
+// //     this.id = a_id
+// //     this.article = a_article
+// //     this.prix = a_prix
+// //     this.qte = a_qte
+
+// //     this.calculerTVA = function(){
+// //         console.log( (this.prix) +(this.prix *20)/100);
+// //     }
+// //  }
+// // const NevAchat= new Achat(5,"voiture",7000000 ,1);
+// // console.log(NevAchat.article);
+// // console.log(NevAchat.calculerTVA());
+
+
+// // class Achat{
+// //     constructor(a_id , a_article , a_prix , a_qte){
+// //         this.id = a_id ;
+// //         this.article = a_article ;
+// //         this.prix = a_prix ;
+// //         this.qte = a_qte ;
+
+// //     }
+// //     calculerTTC(){
+// //         const vTVA =20/100 ;
+// //         let total = this.prix * this.qte ;
+// //         let totalTTC = total = total * vTVA ;
+
+// //         return totalTTC
+// //     }
+// // }
+// // const achat = new Achat(1 , "voiture ", 7000000 , 1);
+// // console.log(achat.calculerTTC());
+
+
+// //object string
+
+
+//  let branche = "DD106"
+//  let module = "develleppement"
+//  let prenom = "NAOUAL"
+//  let nom = "eladnani"
+
+// // let phrase = " I am a web dev" ;
+
+// // console.log(branche.length);
+
+// // console.log(module.charAt(6));
+
+// // console.log(branche.substring(2,5));
+
+// // console.log(prenom.toLowerCase());
+
+// // console.log(nom.toUpperCase());
+
+// // console.log(phrase.split(""));
+
+// // console.log(Array.from(nom));
+
+// // console.log(prenom.startsWith("N_"));
+
+// // console.log(prenom.endsWith(""));
+
+
+
+// //Object date
+
+// let vDate =new Date();
+
+// vDate.setFullYear(2020);
+
+// vDate.setMonth(1);
+
+// console.log(vDate);
+
+// console.log(vDate.getMonth());
+
+// console.log(vDate.getDay());
+
+// console.log(vDate.getHours());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
