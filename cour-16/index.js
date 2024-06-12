d460c77582703e0ee8f05b3fe78721046cc7e426
naoualloli <naoualloli967@gmail.com>
2024-06-12 19:29:54 +0200

    cour-16

---
+++ cour-16/index.js
@@ -0,0 +1,50 @@
+// //Objet Math
+
+// let stagiares =[{id:1, prenom:"naoual" , note:18},
+//                 {id:1, prenom:"hiba" , note:19},
+//                 {id:1, prenom:"sara" , note:6}
+//             ]
+
+// listeNotes= stagiares.map(function(itemValue){
+//     return itemValue.note
+// })
+// let maxNote = Math.max(...listeNotes);
+
+// let stagiaresListeS = stagiares.filter(function(itemValue){
+//     return itemValue.note === maxNote
+// })
+// console.log(stagiaresListeS) 
+
+// console.log(Math.max(...listeNotes))  //akbar note
+
+// console.log("Random Number :")   
+// console.log(Math.floor(Math.random()*20)+1) //kayb9aw ykhtalfo lar9am mn 1 htel 20
+
+// console.log(Math.round(20.6)) //katakhod li 9rib lfassila
+
+// console.log(Math.sqrt(20)) //gider caree
+
+// //Objet Window 
+
+
+// document.querySelector(".btn-show").addEventListener("click",function(){
+//     //open("https://www.google")
+// })
+
+
+
+
+
+
+
+
+// // //SetTimeout methode
+
+// // console.log("1");
+// // setTimeout(function(){
+// //     console.log("2");
+// // },5000)
+// // console.log("3");
+// // console.log("4");
+// // console.log("5");
+
